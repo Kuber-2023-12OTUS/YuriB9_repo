4381d1207dba66ca2a5981fcaa1bcf36fad72200
Yuriy Batkov <yuriy_batkov_external@itms.ru>
2024-05-10 10:55:04 +0300

    homework 11

---
+++ kubernetes-vault/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "cloud_id" {
+  description = "Cloud id where to create the sources"
+  type        = string
+}
+
+variable "folder_id" {
+  description = "Folder id where to create the sources"
+  type        = string
+}
